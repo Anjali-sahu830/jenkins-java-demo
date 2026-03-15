09c291dfe3191a22e13dc87370990b342fe709c0
Anjali-sahu830 <sahusandhya830@gmail.com>
2026-03-15 16:44:07 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
   public static void main(String[] args){
     System.out.println("Hello Jenkins from Github");
-     System.out.println(" Github  new Version");
+     System.out.println(" Github  newwwwwwwwwww Version");
   }
 }
